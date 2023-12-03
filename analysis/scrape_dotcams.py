b411f1da7e29f602d875dce4ef29448f93ec6a36
b arnav <crooks-script.0r@icloud.com>
2023-12-03 15:47:14 -0500

    upload files

---
+++ analysis/scrape_dotcams.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+import time
+import requests
+from selenium.webdriver.common.by import By
+import os
+
+cams_dict = { 'Queensboro_60thst' : 'https://webcams.nyctmc.org/api/cameras/d83aed40-b117-424b-8caf-35c3afe82527/image?t=1697576091936',
+             'BB25' : 'https://webcams.nyctmc.org/api/cameras/4f688c33-672c-4bac-bf2b-98438d464ccd/image?t=1697548827548',
+             '7thave_23st': 'https://webcams.nyctmc.org/api/cameras/fcfbaa3d-13e5-4687-9688-ae1eab37c723/image?t=1697727190132',
+             '8ave_49st' : 'https://webcams.nyctmc.org/api/cameras/4850e464-1111-4b5d-a72d-f54a0e12a789/image?t=1698153162583',
+             'bway_46stN': 'https://webcams.nyctmc.org/api/cameras/f3d82b25-6b10-44c7-ae89-30df075a7ac8/image?t=1698759465348',
+             'bway_51st': 'https://webcams.nyctmc.org/api/cameras/3d8e276f-b179-414d-a506-679d5e559d3e/image?t=1698759641304',
+             'atlantic_vanderbilt' : 'https://webcams.nyctmc.org/api/cameras/b5d8fe4f-1cf5-4cd8-8211-8353e68da1cb/image?t=1698759748728',
+             '4ave_union' : 'https://webcams.nyctmc.org/api/cameras/53d62ac5-34f9-4d8f-82b9-0cecebc9c55a/image?t=1698759703981',
+             'BB44': 'https://webcams.nyctmc.org/api/cameras/26136ff4-a399-4cce-9a37-4d6331f5f1fb/image?t=1699148658436',
+             'BB39': 'https://webcams.nyctmc.org/api/cameras/32651453-d9de-4cde-af4b-be42b8de2775/image?t=1699148698081'
+}
+
+start_time = time.time()
+run_time = 60 * 180     #three hours
+end_time = start_time + run_time  
+
+driver = webdriver.Firefox()
+
+current_index = 0
+cameras_list = list(cams_dict.keys())
+num_urls = len(cameras_list)
+while time.time() < end_time:
+    for index in range(num_urls):
+        cam = cameras_list[current_index]
+        url = cams_dict[cam]    
+        try:
+            driver.get(url)
+            time.sleep(1)
+            
+            image_element = driver.find_element(By.XPATH, "/html/body/img")
+            
+            image_url = image_element.get_attribute("src")
+            
+            response = requests.get(image_url)
+            
+            if response.status_code == 200:
+                timestamp = time.strftime("%Y%m%d_%H%M%S")
+                image_filename = f"{cam}/image_{timestamp}.jpg"
+                if not os.path.exists(f"{cam}/"):
+                    os.makedirs(f"{cam}/")
+                with open(image_filename, "wb") as file:
+                    file.write(response.content)
+                print(f"Saved {image_filename}")
+            
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            
+        time.sleep(.5)
+        current_index = (current_index + 1) % num_urls 
+
+driver.quit()
